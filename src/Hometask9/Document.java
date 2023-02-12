b8bc731b77384ee2dd10049732e01150b1c542d3
KseniyaFilonenko <filonenko_k@tut.by>
2023-02-12 18:33:15 +0300

    Exception task

---
+++ src/Hometask9/Document.java
@@ -0,0 +1,28 @@
+package Hometask9;
+
+import java.util.Date;
+
+public abstract class Document {
+    protected String numberOfDocument;
+    protected Date dateOfDocument;
+
+    public Document() {
+    }
+
+    public Document(String numberOfDocument, Date dateOfDocument) {
+        this.numberOfDocument = numberOfDocument;
+        this.dateOfDocument = dateOfDocument;
+    }
+
+    public Date getDateOfDocument() {
+
+        return dateOfDocument;
+    }
+
+    public String getNumberOfDocument() {
+
+        return numberOfDocument;
+    }
+
+    public abstract String getInfo();
+}
---
+++ src/Hometask9/SupplyContract.java
@@ -0,0 +1,31 @@
+package Hometask9;
+
+import java.util.Date;
+
+/*
+Контракт на поставку товаров. Содержит поля:
+    Номер документа
+    Тип товаров
+    Количество товаров
+    Дата документа
+ */
+public class SupplyContract extends Hometask9.Document {
+
+    private String goodsType;
+    private int goodsCount;
+
+    public SupplyContract() {
+    }
+
+    public SupplyContract(String numberOfDocument, Date dateOfDocument, String goodsType, int goodsCount) {
+        super(numberOfDocument, dateOfDocument);
+        this.goodsType = goodsType;
+        this.goodsCount = goodsCount;
+    }
+
+    @Override
+    public String getInfo() {
+        return goodsType + " Supply Contract № " + numberOfDocument + " of "  + dateOfDocument + ". " +
+                "Amount: " + goodsCount;
+    }
+}
